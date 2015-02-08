8a3e6763bac946f401672bf3bb38ff96a8fe4872
George Merlocco <george@merloc.co>
2015-02-08 03:27:37 -0500

    Initial commit of a 320 x 240 micro cG version

--- install-linux-dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# cryptoGlance - Installs apache if never installed before
-
-sudo apt-get install apache2
-sudo apt-get install php5
-sudo apt-get install libapache2-mod-php5
-sudo apt-get install php5-json
-sudo apt-get install php5-curl
---
+++ json-cryptsy.php
@@ -0,0 +1,14 @@
+<?php
+    $url = "http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=169";
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_FAILONERROR, true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSLVERSION, 4);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; RaspberryPi Ticker; PHP/' . phpversion() . ')');
+    echo curl_exec($curl);
+?>
